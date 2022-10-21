7140fc6080bd71de4c6b340fb49917a5f2636dea
mghlb <79663822+mghlb@users.noreply.github.com>
2022-10-21 21:48:06 +0200

    Solutions to module 1 problems

--- part1/create-image.rkt
+++
@@ -1,9 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname create-image) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-(require 2htdp/image)
-
-(above (rectangle 400 80 "solid" "red")
-       (overlay (star 40 "solid" "gray")
-        (rectangle 400 80 "solid" "white"))
-       (rectangle 400 80 "solid" "black"))
