06f94920d3ae5f96735280bbbce6ff9abf05af37
mghlb <79663822+mghlb@users.noreply.github.com>
2022-11-02 19:19:48 +0200

    Add solution to problems for HtDF

---
+++ 1/boxify.rkt
@@ -0,0 +1,35 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname boxify) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+;; boxify-starter.rkt
+
+; 
+; PROBLEM:
+; 
+; Use the How to Design Functions (HtDF) recipe to design a function that consumes an image, 
+; and appears to put a box around it. Note that you can do this by creating an "outline" 
+; rectangle that is bigger than the image, and then using overlay to put it on top of the image. 
+; For example:
+; 
+; (boxify (ellipse 60 30 "solid" "red")) should produce .
+; 
+; Remember, when we say DESIGN, we mean follow the recipe.
+; 
+; Leave behind commented out versions of the stub and template.
+; 
+
+
+; Image -> Image
+; receives img and return it outlined by a box
+(check-expect (boxify (ellipse 60 30 "solid" "red"))  .)
+
+; (define (boxify img) img)         ;;STUB
+
+; (define (boxify img)              ;;TEMPLATE
+;   (... img))
+
+(define (boxify img)
+  (overlay (rectangle (image-width img) (image-height img) "outline" "black")
+           img))
